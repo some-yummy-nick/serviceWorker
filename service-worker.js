191fe9ba4e0c7fb431ee2cb09694ce7f81bc0895
yeffasol <yeffasol@mail.com>
2018-08-06 15:59:33 +0300

    Add first commit

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+"use strict";
+
+self.addEventListener('fetch', function (event) {
+	// Check if the current request is slow 3g
+	if (/\slow-2g|2g/.test(navigator.connection.effectiveType)) {
+		// Check if the request is for an image
+		if (/\.jpg$|.png$|.gif$|.webp$/.test(event.request.url)) {
+
+			// Return no images
+			event.respondWith(
+				fetch('placeholder.svg', {
+					mode: 'no-cors'
+				})
+			);
+		}
+	}
+});
